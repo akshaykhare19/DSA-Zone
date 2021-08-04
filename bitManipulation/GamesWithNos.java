24b945f84ac89d206d0ffc82fbcd009dcfd3fdca
Akshay Khare <61112118+akshaykhare19@users.noreply.github.com>
2021-08-04 19:20:08 +0530

    Games With Numbers

--- bitManipulation/GamesWithNos.java
+++ bitManipulation/GamesWithNos.java
@@ -1,6 +1,5 @@
 package bitManipulation;
 
-
 /*
  * You are given an array arr[], you have to re-construct an array arr[].
 The values in arr[] are obtained by doing Xor of consecutive elements in the array.
