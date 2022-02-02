a11871c481933ca348ed2b0db818da4c86cf77e4
Akshay Khare <akhare931@gmail.com>
2022-02-02 23:51:33 +0530

    Patterns

---
+++ Pepcoding_DSA/DSA Level 1/Basics Of Programming/Patterns/Pattern13.java
@@ -0,0 +1,69 @@
+/*
+
+Pattern 13
+
+easy
+
+1. You are given a number n.
+
+2. You've to create a pattern as shown in output format
+
+                                 
+
+                                
+
+                               
+
+                               
+Constraints
+
+1 <= n <= 10
+Format
+Input
+
+A number n
+Output
+
+pat131
+Example
+
+Sample Input
+
+5
+Sample Output
+
+1	
+
+1	1	
+
+1	2	1	
+
+1	3	3	1	
+
+1	4	6	4	1	
+
+*/
+
+import java.util.*;
+
+public class Pattern13
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+        {
+            int icj = 1;
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print(icj + "\t");
+                icj = icj * (i-j)/(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
+
+//nc(r+1) = (n-r)/(r+1) * ncr
