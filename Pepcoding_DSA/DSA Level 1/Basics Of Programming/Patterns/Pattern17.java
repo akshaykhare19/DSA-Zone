c9ba926816457813022185354d692e3fce71a95e
Akshay Khare <akhare931@gmail.com>
2022-02-04 23:51:04 +0530

    Patterns

--- Pepcoding_DSA/DSA Level 1/Basics Of Programming/Patterns/Pattern17.java
+++ Pepcoding_DSA/DSA Level 1/Basics Of Programming/Patterns/Pattern17.java
@@ -56,18 +56,15 @@ public class Pattern17
 
         for(int i = 1; i <= n; i++)
         {
-            if(i == n/2 + 1)
+            for(int j = 1; j <= nsp; j++)
             {
-                for(int j = 1; j <= n/2; j++)
-                {
-                    System.out.print("*\t");
+                if(i == n/2 + 1){
+                    System.out.print("*\t");    
                 }
-            }
-            else {
-                for(int j = 1; j <= nsp; j++)
-                {
+                else{
                     System.out.print("\t");
                 }
+                
             }
 
 
