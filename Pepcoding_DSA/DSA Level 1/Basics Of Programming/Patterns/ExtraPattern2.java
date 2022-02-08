79cdf5dd520d0564e8ffac94bbd8ea86aa5e846e
Akshay Khare <akhare931@gmail.com>
2022-02-08 17:18:48 +0530

    Extra Patterns

---
+++ Pepcoding_DSA/DSA Level 1/Basics Of Programming/Patterns/ExtraPattern2.java
@@ -0,0 +1,81 @@
+/*
+
+1               2       3       4
+                1
+1       2       3       4       5
+                1
+1       2       3               4
+
+*/
+
+
+import java.util.Scanner;
+
+public class ExtraPattern2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++)
+        {
+            int count = 1;
+            for(int j = 1; j <= n; j++)
+            {
+                if(i == 1)
+                {
+                    if(j == 1 || j >= n/2 + 1)
+                    {
+                        System.out.print(count++ + "\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+
+                else if(i <= n/2)
+                {
+                    if(j == n/2 + 1)
+                    {
+                        System.out.print(count++ + "\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+
+                else if(i == n/2 + 1)
+                {
+                    System.out.print(count++ + "\t");
+                }
+
+                else if(i > n/2 + 1 && i != n)
+                {
+                    if(j == n/2 + 1)
+                    {
+                        System.out.print(count++ + "\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+
+                else if(i == n)
+                {
+                    if(j == n || j <= n/2 + 1)
+                    {
+                        System.out.print(count++ + "\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+        
+    }
+}
