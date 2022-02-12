4643a618f30b38c746bc42430112bf67c3bd12ec
Akshay Khare <akhare931@gmail.com>
2022-02-12 23:22:00 +0530

    Any base addition and subtraction

--- Pepcoding_DSA/DSA Level 1/Basics Of Programming/Function and Arrays/AnyBaseAddition.java
+++ Pepcoding_DSA/DSA Level 1/Basics Of Programming/Function and Arrays/AnyBaseAddition.java
@@ -1,3 +1,47 @@
+/*
+
+Any Base Addition
+
+easy
+
+1. You are given a base b.
+
+2. You are given two numbers n1 and n2 of base b.
+
+3. You are required to add the two numbes and print their value in base b.
+Constraints
+
+2 <= b <= 10
+
+0 <= n1 <= 256
+
+0 <= n2 <= 256
+Format
+Input
+
+A base b
+
+A number n1
+
+A number n2
+Output
+
+A number representing the sum of n1 and n2 in base b.
+Example
+
+Sample Input
+
+8
+
+777
+
+1
+Sample Output
+
+1000
+
+*/
+
 import java.util.*;
 
 public class AnyBaseAddition
@@ -17,7 +61,7 @@ public class AnyBaseAddition
     private static int getSum(int b, int n1, int n2)
     {
         int ans = 0, placeVal, carry = 0, power = 1;
-        while(n1 > 0 || n2 > 0){
+        while(n1 > 0 || n2 > 0 || carry != 0){
             int d1 = n1%10;
             int d2 = n2%10;
             placeVal = (d1 + d2 + carry) % b;
