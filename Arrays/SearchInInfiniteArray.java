d4e2b3bb6c54d7e519e92908c3f1aa6402830781
Akshay Khare <akhare931@gmail.com>
2022-06-21 23:09:57 +0530

    Infinite search

---
+++ Arrays/SearchInInfiniteArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class SearchInInfiniteArray
+{
+    //TC - O(log n)
+    //SC - O(1)
+    public static int infiniteSearch(int inf[], int key)
+    {
+        int low = 0;
+        int high = 1;
+        while(inf[high] < key)
+        {
+            low = high;
+            //exponential growth
+            high = 2*high;
+        }
+        return Arrays.binarySearch(inf, low, high, key);
+    }
+
+    public static void main(String args[])
+    {
+        int a[] = {19,20,22,224,225,335,670,700,701,702,703,704,705};
+        int key = 223;
+        System.out.println(infiniteSearch(a, key));
+    }    
+}
