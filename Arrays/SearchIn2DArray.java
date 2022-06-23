74293ff9a2a6deb1f990919c5ecd746dc2a83969
Akshay Khare <akhare931@gmail.com>
2022-06-24 00:12:49 +0530

    Search in sorted 2d array

---
+++ Arrays/SearchIn2DArray.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class SearchIn2DArray
+{
+    public static ArrayList<Integer> search(int a[][], int key)
+    {
+        ArrayList<Integer> al = new ArrayList<>();
+        int i = 0, j = a.length-1;
+        while(i < a.length && j >= 0){
+            if(a[i][j] == key){
+                al.add(i);
+                al.add(j);
+                return al;
+            }
+            else if(a[i][j] < key){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return al;
+    }
+
+    public static void main(String args[])
+    {
+        int a[][] = {{1,2,3,4},
+                      {11,12,13,14},
+                      {21,22,23,24},
+                      {31,32,32,34}};
+        int key = 22;
+        ArrayList<Integer> ans = search(a, key);
+        System.out.println(ans);
+    }    
+}
