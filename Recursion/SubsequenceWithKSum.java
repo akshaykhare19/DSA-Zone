2804af2196c32fab96e36661f128b462dffe4b74
Akshay Khare <akhare931@gmail.com>
2022-08-04 19:04:20 +0530

    Subsequences

--- Recursion/SubsequenceWithKSum.java
+++ Recursion/SubsequenceWithKSum.java
@@ -1,26 +1,32 @@
 import java.util.*;
 public class SubsequenceWithKSum
 {
-    private static void f(int idx, List<Integer> ds, int sum, int k, int a[])
+    // Any one subsequence with sum k
+    private static boolean f(int idx, List<Integer> ds, int sum, int k, int a[])
     {
-        
+
         if(idx==a.length){
-            if(sum==k) System.out.println(ds);
-            return;
+            if(sum==k) {
+                System.out.println(ds);
+                return true;
+            }
+            return false;
         }
 
         ds.add(a[idx]);
         sum+=a[idx];
-        f(idx+1, ds, sum, k, a);
+        if(f(idx+1, ds, sum, k, a)) return true;
+        // if(isFound) return;
         ds.remove(ds.size()-1);
         sum-=a[idx];
-        f(idx+1, ds, sum, k, a);
+        if(f(idx+1, ds, sum, k, a)) return true;
+        return false;
     }
 
     public static void main(String args[])
     {
         List<Integer> ans = new ArrayList<>();
-        int a[] = {1,2,1};
-        f(0, ans, 0, 2, a);
+        int a[] = {1,2,1,4,3};
+        f(0, ans, 0, 5, a);
     }
 }
